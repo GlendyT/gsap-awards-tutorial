52241925884a6d20d2acfd2901ee7efae2e0a2ac
GlendyT <glendytuyuc@gmail.com>
2025-07-30 08:10:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
+
+gsap.registerPlugin(ScrollTrigger);
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <HeroSection />
+      <MessageSection />
+      <FlavorSection />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/sections/FlavorSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlavorSection = () => {
+  return (
+    <div>FlavorSection</div>
+  )
+}
+
+export default FlavorSection
